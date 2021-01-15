a476c6336114b015b17b82390622c5710111d6c7
Muyinda ROgers <moverr@gmail.com>
2021-01-15 16:08:11 +0300

    :Debuggging

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -1,9 +1,9 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{AbstractController, ControllerComponents}
 
-class AuthController @Inject()(cc: ControllerComponents)  extends  BaseController(cc){
+class AuthController @Inject()(cc: ControllerComponents)  extends AbstractController(cc){
 
   def  login(): Unit ={
     TODO
