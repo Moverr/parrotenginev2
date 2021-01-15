52088dab690de92ed5bb7a4f414e2ee21706a1ec
Muyinda ROgers <moverr@gmail.com>
2021-01-15 14:41:14 +0300

    Auth COntroller, Login and Register

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -1,5 +1,14 @@
 package controllers
 
-class AuthController {
+import javax.inject.Inject
+import play.api.mvc.{BaseController, ControllerComponents}
 
+class AuthController @Inject()(cc: ControllerComponents)  extends  BaseController(cc){
+
+  def  login(): Unit ={
+    TODO
+  }
+  def register(): Unit ={
+    TODO
+  }
 }
