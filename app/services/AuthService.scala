7249e90be027acf4c9d3222d03506c5547d84198
Muyinda ROgers <moverr@gmail.com>
2021-01-16 14:45:59 +0300

    :Debuggging

--- app/services/AuthService.scala
+++ app/services/AuthService.scala
@@ -2,11 +2,14 @@ package services
 
 import controllers.requests.LoginRequest
 import controllers.responses.LoginResponse
+import  collection.mutable
 
 class AuthService {
+  private val users = mutable.Map[String,String]("rogers"->"moaoe")
+
   //todo: Login Function
   def validate(loginRequest: LoginRequest): LoginResponse ={
-
+   val user =  users.get(loginRequest.username).exists(_ == loginRequest.password)
     null
   }
 
