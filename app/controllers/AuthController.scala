7aaf819c934f70305d490c1efae942af60a932b1
Muyinda ROgers <moverr@gmail.com>
2021-01-16 13:23:02 +0300

    :Debugging

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -8,8 +8,11 @@ class AuthController @Inject()(cc:ControllerComponents)   extends AbstractContro
 
   def login() = Action {request=>
     val loginVals = request.body.asJson
+    loginVals.map{ arg=>
+      Ok("Passing"+loginVals)
+    }.getOrElse(Ok("Invalid Entries "))
 
-    Ok("Passing"+loginVals)
+
   }
 
   def register(): Unit ={
