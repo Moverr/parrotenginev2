c73378f7acb00254b09981d717f3c0e45bb168d2
Muyinda ROgers <moverr@gmail.com>
2021-01-17 13:20:39 +0300

    :Debuggging

--- app/services/AuthService.scala
+++ app/services/AuthService.scala
@@ -5,13 +5,19 @@ import controllers.responses.LoginResponse
 import javax.inject.Inject
 import play.api.db.slick.DatabaseConfigProvider
 import slick.jdbc.JdbcProfile
+import slick.lifted.TableQuery
+import tables.Account
 
-import collection.mutable
+import scala.collection.mutable
 
 class AuthService @Inject()(dbConfigProvider: DatabaseConfigProvider) {
   private val users = mutable.Map[String,String]("rogers"->"moaoe")
 
   private  val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+
+  val account =  TableQuery[Account]
 
   //todo: Login Function
   def validate(loginRequest: LoginRequest): LoginResponse ={
