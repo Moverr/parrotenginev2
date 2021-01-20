e27179231d026546c0c1221a1a14e21ef33f3030
Muyinda ROgers <moverr@gmail.com>
2021-01-20 07:09:02 +0300

    UserTable

--- app/db/tables/User.scala
+++ app/db/tables/User.scala
@@ -1,3 +1,10 @@
 package db.tables
 
+import slick.jdbc.PostgresProfile.api._
+//import defaults ::
+
 case  class User(id:Long = 0L,username:String,password:String,account: Option[Int])
+
+class UserTable(tag: Tag) extends Table[User](tag,"users"){
+  override def * = ???
+}
