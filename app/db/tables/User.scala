20e271991b04c5a6cce7250902dd2e55516d64fb
Muyinda ROgers <moverr@gmail.com>
2021-01-20 07:13:00 +0300

    :Debuggging

--- app/db/tables/User.scala
+++ app/db/tables/User.scala
@@ -3,8 +3,12 @@ package db.tables
 import slick.jdbc.PostgresProfile.api._
 //import defaults ::
 
-case  class User(id:Long = 0L,username:String,password:String,account: Option[Int])
+case  class User(id:Long = 0L,username:String,password:String,account: Option[Long])
 
 class UserTable(tag: Tag) extends Table[User](tag,"users"){
+  def id       = column[Long]("id",O.PrimaryKey,O.AutoInc)
+  def username = column[String]("username",O.Unique)
+  def password = column[String]("password")
+  def account  = column[Long]("account")
   override def * = ???
 }
