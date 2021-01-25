50ecbf801c6b126d73ef2bdfe8f94d4eebdaab9e
Muyinda ROgers <moverr@gmail.com>
2021-01-25 20:47:52 +0300

    :Debuggging: Future Asychronous Controller

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 import services.AuthService
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 class AuthController @Inject()(
@@ -16,13 +17,20 @@ class AuthController @Inject()(
 
   def login() = Action.async { implicit request: Request[AnyContent] =>
     val body = request.body
+    val jsonBody= body.asJson
 
-    val username = "dd"
-    val password = "ddd"
-    val loginRequest = LoginRequest(username, password)
-    authService.validate(loginRequest) map { items =>
-      Ok(items.password.toString)
+    jsonBody.map{x =>
+      val username = "dd"
+      val password = "ddd"
+      val loginRequest = LoginRequest(username, password)
+      authService.validate(loginRequest) map { items =>
+        Ok(x.toString)
+      }
+    }.getOrElse{
+      Future.successful(Ok("Scenario Fake "))
     }
+
+
 
     }
 
