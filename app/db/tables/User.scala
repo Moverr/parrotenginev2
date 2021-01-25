a18eb58125bf5d87860cf8e07120d41fb770232a
Muyinda ROgers <moverr@gmail.com>
2021-01-25 20:15:28 +0300

    Bla configuration

--- app/db/tables/User.scala
+++ app/db/tables/User.scala
@@ -3,12 +3,12 @@ package db.tables
 import slick.jdbc.PostgresProfile.api._
 //import defaults ::
 
-case  class User(id:Long = 0L,username:String,password:String,account: Option[Long])
+case  class User(id:Long = 0L,username:String,password:String)
 
 class UserTable(tag: Tag) extends Table[User](tag,"users"){
   def id       = column[Long]("id",O.PrimaryKey,O.AutoInc)
   def username = column[String]("username",O.Unique)
   def password = column[String]("password")
-  def account  = column[Long]("account",O.Default(0))
-  override def * = (id,username,password,account.?) <> (User.tupled,User.unapply)
+  //def account  = column[Long]("account",O.Default(0))
+  override def * = (id,username,password) <> (User.tupled,User.unapply)
 }
