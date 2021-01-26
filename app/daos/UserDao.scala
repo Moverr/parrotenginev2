c564edbf95c3210fae891835b7662856ec9f9967
Muyinda ROgers <moverr@gmail.com>
2021-01-26 13:15:27 +0300

    :Debuggging

--- app/daos/UserDao.scala
+++ app/daos/UserDao.scala
@@ -1,5 +1,29 @@
 package daos
 
-class UserDao {
+import db.tables.{User, UserTable}
+import javax.inject.{Inject, Singleton}
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.TableQuery
 
+import scala.concurrent.Future
+
+@Singleton
+class UserDao @Inject()(dbConfigProvider: DatabaseConfigProvider) {
+  private  val dbConfig = dbConfigProvider.get[JdbcProfile]
+  lazy  val UserTable = TableQuery[UserTable]
+  import dbConfig._
+
+  def getUserByName(useername:String): Future[Option[User]] ={
+    val query = UserTable.filter(_.username === useername)
+      .result.headOption
+      db.run(query)
+  }
+
+  def getUsersByUsername(useername:String,offset:Int,limit:Int): Future[Seq[User]] ={
+    val query = UserTable.filter(_.username === useername)
+      .result
+    db.run(query)
+  }
 }
