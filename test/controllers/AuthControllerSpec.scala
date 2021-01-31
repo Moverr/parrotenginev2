ae9b4fc5da7c813e8e854b47b3fba3ca3b125ea1
Muyinda ROgers <moverr@gmail.com>
2021-01-31 21:39:19 +0300

    Tests builder

--- test/controllers/AuthControllerSpec.scala
+++ test/controllers/AuthControllerSpec.scala
@@ -1,7 +1,21 @@
 package controllers
 import org.scalatest._
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 
+class ExampleSpec extends PlaySpec with GuiceOneAppPerSuite {
 
-val application: Application = new GuiceApplicationBuilder()
-.configure("some.configuration" -> "value")
-.build()
+  // Override fakeApplication if you need a Application with other than
+  // default parameters.
+  override def fakeApplication(): Application = {
+    GuiceApplicationBuilder().configure(Map("ehcacheplugin" -> "disabled")).build()
+  }
+
+  "The GuiceOneAppPerSuite trait" must {
+    "provide an Application" in {
+      app.configuration.getOptional[String]("ehcacheplugin") mustBe Some("disabled")
+    }
+  }
+}
