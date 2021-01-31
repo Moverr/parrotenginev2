9685a91d885f2257915fe1c027556de3f199cb5d
Muyinda ROgers <moverr@gmail.com>
2021-01-31 17:38:33 +0300

    :Debugging

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -5,8 +5,8 @@ import javax.inject.{Inject, Singleton}
 import play.api.mvc._
 import services.AuthService
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class AuthController @Inject()(
@@ -18,7 +18,7 @@ class AuthController @Inject()(
   def login() = Action.async { implicit request: Request[AnyContent] =>
 
     val username:String = request.body.asFormUrlEncoded.flatMap(m => m.get("username").flatMap(_.headOption)).getOrElse("")
-    val password:String =  request.body.asFormUrlEncoded.flatMap(m => m.get("name").flatMap(_.headOption)).getOrElse("")
+    val password:String =  request.body.asFormUrlEncoded.flatMap(m => m.get("password").flatMap(_.headOption)).getOrElse("")
     val loginRequest = LoginRequest(username, password)
 
     authService.validate(loginRequest)
@@ -26,7 +26,6 @@ class AuthController @Inject()(
         case Some(value) => Future.successful(Ok( value.access_token))
         case None => Future.successful(BadRequest("Something went wrong"))
       }
-
     }
 
   def register(): Unit ={
