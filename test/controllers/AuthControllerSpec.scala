8b4c09823844754171f1a7536ba5fd239d4862bf
Muyinda ROgers <moverr@gmail.com>
2021-01-31 18:11:21 +0300

    :Deebuggging

---
+++ test/controllers/AuthControllerSpec.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import org.scalatest.FunSuite
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.Injecting
+
+class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting{
+
+}
