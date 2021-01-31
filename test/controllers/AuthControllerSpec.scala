5dd0899f9b9b0caa4c9d02d1e88d6ed5dd48164d
Muyinda ROgers <moverr@gmail.com>
2021-01-31 21:32:49 +0300

    :Debuggging

--- test/controllers/AuthControllerSpec.scala
+++ test/controllers/AuthControllerSpec.scala
@@ -1,20 +1,7 @@
 package controllers
+import org.scalatest._
 
-import javax.inject.Inject
-import org.scalatest.FunSuite
-import org.scalatestplus.play.PlaySpec
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.mvc.ControllerComponents
-import play.api.test.Helpers.stubControllerComponents
-import play.api.test.Injecting
 
-class AuthControllerSpec @Inject()(cc:ControllerComponents)
-  extends PlaySpec with GuiceOneAppPerTest with Injecting{
-  "Authorization Controller " should{
-    "Mock Loging Experience " in {
-        val x = new  AuthController(stubControllerComponents(),null)
-    }
-
-  }
-
-}
+val application: Application = new GuiceApplicationBuilder()
+.configure("some.configuration" -> "value")
+.build()
--- test/controllers/HomeControllerSpec.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest._
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
-
-
-val application: Application = new GuiceApplicationBuilder()
-  .configure("some.configuration" -> "value")
-  .build()
