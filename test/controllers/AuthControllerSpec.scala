e64e57abadbde74fc4c3d7c77f2549fb496854f7
Muyinda ROgers <moverr@gmail.com>
2021-02-02 13:41:07 +0300

    :Debuggigng

--- test/controllers/AuthControllerSpec.scala
+++ test/controllers/AuthControllerSpec.scala
@@ -2,13 +2,14 @@ package controllers
 import org.scalatest._
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.Helpers._
 import play.api.test.Injecting
 
 class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "Example Page#index" should {
     "should be valid" in {
-     // val controller = new AuthController(stubControllerComponents())
+      val controller = new HomeController(stubControllerComponents())
     }
   }
 }
