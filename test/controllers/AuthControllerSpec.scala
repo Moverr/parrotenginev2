6c070837e797bb0f31f1be24fd6dde644aa55cf6
Muyinda ROgers <moverr@gmail.com>
2021-02-02 21:10:21 +0300

    Using Mockito

--- test/controllers/AuthControllerSpec.scala
+++ test/controllers/AuthControllerSpec.scala
@@ -1,15 +1,22 @@
 package controllers
+import daos.UserDao
 import org.scalatest._
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.Helpers._
 import play.api.test.Injecting
+import services.AuthService
+
 
 class AuthControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "Example Page#index" should {
     "should be valid" in {
-      val controller = new HomeController(stubControllerComponents())
+
+      val userDao =  new UserDao(null)
+      val authService = new AuthService(userDao)
+
+      val controller = new AuthController(stubControllerComponents(),authService)
     }
   }
 }
