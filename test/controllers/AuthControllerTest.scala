9dd0546164a74343b460a7f9167d6bbf14dd402b
Muyinda ROgers <moverr@gmail.com>
2021-02-07 20:22:11 +0300

    :Debugging : Fn Test

---
+++ test/controllers/AuthControllerTest.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import services.IAuthService
+
+class AuthControllerTest  (authservice: IAuthService) extends PlaySpec {
+
+  "AuthControllerTest" should {
+    "login" in {
+
+    }
+
+    "register" in {
+
+    }
+  }
+}
