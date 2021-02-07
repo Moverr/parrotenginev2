1cef25236e8d45dc6a3d6db64a401d66b5c5b721
Muyinda ROgers <moverr@gmail.com>
2021-02-07 20:44:37 +0300

    :Debuggigng

--- test/controllers/AuthControllerTest.scala
+++ test/controllers/AuthControllerTest.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import org.scalatestplus.play.PlaySpec
+import play.api.test.FakeRequest
 import services.IAuthService
 import play.api.test.Helpers._
 
@@ -9,6 +10,8 @@ class AuthControllerTest  (authservice: IAuthService) extends PlaySpec {
   "AuthControllerTest" should {
     "login" in {
       val controller = new AuthController(stubControllerComponents(),authservice )
+      val home = controller.login().apply(FakeRequest(POST, "/"))
+
     }
 
     "register" in {
