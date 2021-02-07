a4f1fbb73bffc6e0cd412ccef6725c2c6707563d
Muyinda ROgers <moverr@gmail.com>
2021-02-07 22:34:04 +0300

    :Debugging, test out

--- test/controllers/AuthControllerTest.scala
+++ test/controllers/AuthControllerTest.scala
@@ -4,21 +4,19 @@ import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import services.IAuthService
 
-class AuthControllerTest  (authservice: IAuthService) extends PlaySpec {
+class AuthControllerTest  extends PlaySpec   {
 
   "AuthControllerTest" should {
     "login" in {
+       val controller = new AuthController(stubControllerComponents(), null)
       val json = Json.parse("{\"firstName\":\"Foo\", \"lastName\":\"Bar\", \"age\":13}")
 
-      val controller = new AuthController(stubControllerComponents(),authservice )
+
       val request = FakeRequest(POST, "/v1/auth/login").withJsonBody(json)
-
-
-
       val home = controller.login().apply(request)
 
+      status(home) mustBe OK
     }
 
     "register" in {
