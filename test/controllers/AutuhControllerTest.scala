1778186bf4ed74e2d42e838ef443a40d7d6e432e
Muyinda ROgers <moverr@gmail.com>
2021-02-07 20:17:33 +0300

    :Debuggging Test

--- test/controllers/AutuhControllerTest.scala
+++ test/controllers/AutuhControllerTest.scala
@@ -2,18 +2,16 @@ package controllers
 import daos.UserDao
 import org.scalatest._
 import org.scalatestplus.play.PlaySpec
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.Helpers._
-import play.api.test.Injecting
 import services.IAuthService
 
 
 
 
-class AuthControllerSpec(authservice: IAuthService) extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class AutuhControllerTest(authservice: IAuthService) extends PlaySpec  {
 
-  "Example Page#index" should {
-    "should be valid" in {
+  "EAuth Controller" should {
+    "Login Client " in {
 
       val userDao =  new UserDao(null)
       val controller = new AuthController(stubControllerComponents(),authservice)
