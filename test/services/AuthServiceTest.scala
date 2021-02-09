6e469b5110b35de1ff7753a87bc402de3f54d383
Muyinda ROgers <moverr@gmail.com>
2021-02-09 05:39:53 +0300

    Debugugging

--- test/services/AuthServiceTest.scala
+++ test/services/AuthServiceTest.scala
@@ -1,7 +1,12 @@
 package services
 
+import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 
-class AuthServiceTest extends PlaySpec {
-
+class AuthServiceTest extends PlaySpec with MockitoSugar {
+  "Auth service" should {
+    "returun user when login is succesful" in {
+      assert(true)
+    }
+  }
 }
