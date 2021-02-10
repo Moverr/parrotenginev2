222c4196fd436cdd244be96c8232e8599e3b7d9a
Muyinda ROgers <moverr@gmail.com>
2021-02-10 12:53:00 +0300

    :Debuggging

--- test/services/AuthServiceTest.scala
+++ test/services/AuthServiceTest.scala
@@ -1,16 +1,17 @@
 package services
 
-import daos.IUserDAO
+import daos.UserDao
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 
 class AuthServiceTest extends PlaySpec with MockitoSugar {
   "Auth service" should  {
     "returun user when login is succesful" in {
-      val userDao = mock[IUserDAO]
+      val userDao = mock[UserDao]
       userDao.getUserByName("sddd").mustBe(null)
-
-      assert(true)
+        val authService = new AuthService(userDao)
+      authService.validate(null) mustBe(null)
+ 
     }
   }
 }
