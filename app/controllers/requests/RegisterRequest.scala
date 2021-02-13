ceec9331f371d0ddb36157f80bb15229f70af2ec
Muyinda ROgers <moverr@gmail.com>
2021-02-13 23:59:36 +0300

    :Debuggigng

--- app/controllers/requests/RegisterRequest.scala
+++ app/controllers/requests/RegisterRequest.scala
@@ -1,3 +1,9 @@
 package controllers.requests
 
-case class RegisterRequest()
+
+
+sealed trait   Registration
+
+case class NormalRegistration(  email:String,   password:String)  extends  Registration
+case class InviteRegistration(  author:Long,   email:String)  extends  Registration
+case class SocialAuthRegistration(  token:String)  extends  Registration
