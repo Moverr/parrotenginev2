23bcf7f5951e905ed234237e4084a1651412f68d
Muyinda ROgers <moverr@gmail.com>
2021-02-21 16:40:17 +0300

    :Debugugging

--- test/controllers/AuthControllerTest.scala
+++ test/controllers/AuthControllerTest.scala
@@ -3,16 +3,19 @@ package controllers
 import com.google.inject.Inject
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.mvc.{ControllerComponents, Results}
 import play.api.test._
+import services.AuthService
 
 
 
-class AuthControllerTest  extends PlaySpec  with GuiceOneAppPerSuite   {
-
+class AuthControllerTest @Inject()(authservice:AuthService) extends PlaySpec  with Results   {
 
 
   "AuthControllerTest" should {
     "login"   in  {
+      val controller = new AuthController(Helpers.stubControllerComponents(), authservice)
+      
     /*   val controller = new AuthController(stubControllerComponents(), null)
       val json = Json.parse("{\"firstName\":\"Foo\", \"lastName\":\"Bar\", \"age\":13}")
 
