05f990c85fa9890a87f1b081ea7e3c63a8f28b3b
Muyinda ROgers <moverr@gmail.com>
2021-02-21 18:12:45 +0300

    :Debugging : Testing login payload

--- test/controllers/AuthControllerTest.scala
+++ test/controllers/AuthControllerTest.scala
@@ -4,15 +4,17 @@ import com.google.inject.Inject
 import db.tables.User
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.mvc.{AnyContent, ControllerComponents, Results}
+import play.api.mvc.{AnyContent, ControllerComponents, EssentialAction, Result, Results}
 import play.api.test._
+import play.api.test.Helpers._
+
 import services.AuthService
 import org.mockito.Mockito._
 import play.api.libs.json.Json
 import play.test
 import play.test.Helpers._
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 
 
 class AuthControllerTest  @Inject()(authService:AuthService) extends PlaySpec     {
@@ -27,10 +29,27 @@ class AuthControllerTest  @Inject()(authService:AuthService) extends PlaySpec   
       when(authService.validate(null)).thenReturn( Future.successful(authService.populateResponse(user)))
       val controller = new AuthController(Helpers.stubControllerComponents(), authService)
       val json = Json.parse("{\"username\":\"username\", \"password\":\"password\" }")
-      val request:FakeRequest[AnyContent] = FakeRequest(POST, "/v1/auth/login").withJsonBody(json)
-      val result = controller.login().apply(request)
 
-      Helpers.status(result) mustBe Results.Ok
+
+      val result: Future[Result] = controller.login().apply(FakeRequest(Helpers.POST, "/v1/auth/login").withJsonBody(json))
+
+
+      val bodyText: String = contentAsString(result)
+      bodyText mustBe "ok"
+
+    /*  val request:FakeRequest[AnyContent] = FakeRequest(POST, "/v1/auth/login").withJsonBody(json)
+
+
+      val action: EssentialAction = Action { request =>
+        val value = (request.body.asJson.get \ "field").as[String]
+        Ok(value)
+      }
+
+
+
+      val  result = Helpers.call(action,request)
+      */
+
     /*   val controller = new AuthController(stubControllerComponents(), null)
       val json = Json.parse("{\"firstName\":\"Foo\", \"lastName\":\"Bar\", \"age\":13}")
 
