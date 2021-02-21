3da4c71e10b00f99325386072f68e6f592c98356
Muyinda ROgers <moverr@gmail.com>
2021-02-21 18:27:53 +0300

    :Debuggging

--- test/controllers/AuthControllerTest.scala
+++ test/controllers/AuthControllerTest.scala
@@ -2,16 +2,15 @@ package controllers
 
 import com.google.inject.Inject
 import db.tables.User
+import org.mockito.Mockito
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.mvc.{AnyContent, ControllerComponents, EssentialAction, Result, Results}
 import play.api.test._
 import play.api.test.Helpers._
-
 import services.AuthService
+import org.mockito.Mockito._
 import play.api.libs.json.Json
-
-//todo: Import mockito
 import org.mockito.Mockito._
 
 import scala.concurrent.{Await, Future}
@@ -19,14 +18,14 @@ import scala.concurrent.{Await, Future}
 
 class AuthControllerTest extends PlaySpec     {
 
-  val authService:AuthService = new AuthService(null,null)
+  val authService:AuthService = Mockito.mock(classOf[AuthService])
   //val authService = new AuthService(null,null)
   val user:User  = new User(1,"username","password")
 
   "AuthControllerTest" should {
     "login"   in  {
 
-      when(authService.validate(null))( Future.successful(authService.populateResponse(user)))
+      when(authService.validate(null)).thenReturn( Future.successful(authService.populateResponse(user)))
       val controller = new AuthController(Helpers.stubControllerComponents(), authService)
       val json = Json.parse("{\"username\":\"username\", \"password\":\"password\" }")
 
