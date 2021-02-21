3528ace993a9433d23dcf80cc8ee749a28d925a1
Muyinda ROgers <moverr@gmail.com>
2021-02-21 06:47:05 +0300

    :Debugging

--- test/utitlities/JwtUtilityTest.scala
+++ test/utitlities/JwtUtilityTest.scala
@@ -7,14 +7,12 @@ class JwtUtilityTest extends PlaySpec {
 
   "Test KeySize" should{
     "should be 2048 " in {
-
       JwtUtility.keySize mustBe(2048)
     }
   }
 
 
-  "Test generateKey" should{
-
+  "Test generateKey" should{ 
     "Should return the original text after encryption  " in {
       JwtUtility.retrievPasswordPair(JwtUtility.generateKey("key")) mustBe("key")
     }
