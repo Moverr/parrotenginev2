82b7e237a2ea98edd47c1a66ac7412cde7ad1eab
Muyinda ROgers <moverr@gmail.com>
2021-02-23 21:09:08 +0300

    :ebuggging : Match Regex

--- app/helpers/Utilities.scala
+++ app/helpers/Utilities.scala
@@ -9,7 +9,7 @@ import java.util.regex.Pattern
 object Utilities {
 
   val emailRegex:String = "^(.+)@(.+)$"
-
+  val phoneRegex:String = "^(.+)@(.+)$"
   val mapper = new ObjectMapper() with ScalaObjectMapper
 
   mapper.registerModule(DefaultScalaModule)
@@ -21,12 +21,19 @@ object Utilities {
   }
 
 
-  def encrypt(s:String): String =  MessageDigest.getInstance(matchEncryption(0)).digest(s.getBytes)      .map(_.toChar).mkString
+  def matchRegex(matchType:String) : String = matchType match {
+    case "email"=> emailRegex
+    case "phone"=> phoneRegex
+    case _=>""
+  }
 
+
+
+  def encrypt(s:String): String =  MessageDigest.getInstance(matchEncryption(0)).digest(s.getBytes) .map(_.toChar).mkString
 
   def fromJson[T](json: String)(implicit m: Manifest[T]): T = mapper.readValue[T](json)
 
-  def validateRegexString(matchString: String, regex:String):Boolean= Pattern.compile(regex).matcher(matchString).matches()
+  def validateRegexString(matchString: String, matchType:String):Boolean= Pattern.compile(matchRegex(matchType)).matcher(matchString).matches()
 
 
 }
