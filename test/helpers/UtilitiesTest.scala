77cac1327f85073a2b0992e74f1c0f814e74c665
Muyinda ROgers <moverr@gmail.com>
2021-02-23 22:17:51 +0300

    :Debuggging - from json conversion

--- test/helpers/UtilitiesTest.scala
+++ test/helpers/UtilitiesTest.scala
@@ -1,7 +1,11 @@
 package helpers
 
+import controllers.requests.LoginRequest
+import controllers.responses.AuthResponse
+import db.tables.User
 import org.scalatest.FunSuite
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
 
 class UtilitiesTest extends PlaySpec {
 
@@ -27,6 +31,20 @@ class UtilitiesTest extends PlaySpec {
     }
 
   }
+
+
+  "fromJson" should{
+    val jsonBoy = Json.parse("{\"username\":\"username\", \"password\":\"password\" }")
+
+
+    "Should  convert from json to a case class succesfully" in {
+     Utilities.fromJson[LoginRequest](jsonBoy.toString()).password mustBe("password")
+    }
+
+
+
+  }
+
 
 
 }
