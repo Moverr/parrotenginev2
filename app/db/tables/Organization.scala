d8930ebb8a5dd172275d3ab43ffbff7b6d9b789c
Muyinda ROgers <moverr@gmail.com>
2021-02-26 21:03:08 +0300

    :DDebuggging

--- app/db/tables/Organization.scala
+++ app/db/tables/Organization.scala
@@ -4,17 +4,21 @@ import java.sql.Timestamp
 
 import slick.jdbc.PostgresProfile.api._
 import slick.sql.SqlProfile.ColumnOption.SqlType
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.TableQuery
 
 case class Organization(id:Long = 0L, name:String, details:String,owner:Long, date_created:Timestamp, date_updated:Timestamp)
 
+
 class OrganizationTable(tag: Tag) extends Table[Organization](tag,"organisations"){
+  val UserTable: TableQuery[UserTable]
 
   def id = column[Long]("id",O.PrimaryKey, O.AutoInc)
   def name = column[String]("name")
   def details = column[String]("details")
-  def ownerid =  column("owner",)
-  def owner =  foreignKey("owner")
+  def owner =  column[Long]("owner")
+  def ownerfk =  foreignKey("owner_fk",owner,UserTable)
   def date_created = column[Timestamp]("date_updated", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
   def date_updated = column[Timestamp]("date_updated", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
-  override def * = (id.?,name,details,date_created,date_updated).mapTo[Organization]
+  override def * = (id.?,name,details,owner,date_created,date_updated).mapTo[Organization]
 }
