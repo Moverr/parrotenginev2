4d51a3b690bd7908df8a85703e02be981ffea749
Muyinda ROgers <moverr@gmail.com>
2021-02-26 22:33:54 +0300

    :Debuggging : Organisation DAO

--- app/daos/OrganisationDAO.scala
+++ app/daos/OrganisationDAO.scala
@@ -1,8 +1,20 @@
 package daos
 
+import db.tables.{OrganizationTable, UserTable}
 import javax.inject.Inject
 import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.lifted.TableQuery
 
 class OrganisationDAO  @Inject()(dbConfigProvider: DatabaseConfigProvider) {
 
+
+  private  val dbConfig = dbConfigProvider.get[JdbcProfile]
+  lazy  val orgTable = TableQuery[OrganizationTable]
+  import dbConfig._
+
+
+  //todo: fetch organisation by owner
+
+  //todo: fetch
 }
