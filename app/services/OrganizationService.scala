bb0c0c2481ddbd709f9621b3a28f625f7b0fda9b
Muyinda ROgers <moverr@gmail.com>
2021-02-28 05:01:28 +0300

    :Debggging

--- app/services/OrganizationService.scala
+++ app/services/OrganizationService.scala
@@ -9,18 +9,21 @@ import utitlities.JwtUtility
 import scala.concurrent.Future
 
 @Singleton
-class OrganizationService  @Inject()(organisationDAO: OrganisationDAO)  {
+class OrganizationService  @Inject()(organisationDAO: OrganisationDAO,authService: AuthService)  {
 
   //todo: create organisation
-  def create(authorization:AuthResponse,request:OrganisationRequest): Future[OrganisationResponse] ={
+  def create(token:String,request:OrganisationRequest): Future[OrganisationResponse] ={
+     val auth:Future[Option[AuthResponse]] =  authService.validate(token)
     ???
   }
   //todo: list organinsations
-  def list(authorization:AuthResponse,limit:Int, offset:Int): Future[Seq[OrganisationResponse]]  = {
+  def list(token:String,limit:Int, offset:Int): Future[Seq[OrganisationResponse]]  = {
+    val auth:Future[Option[AuthResponse]] =  authService.validate(token)
     ???
   }
   //todo: Get Organization
-  def get(authorization:AuthResponse,id:Int,): Future[Option[OrganisationResponse]] ={
+  def get(token:String,id:Int,): Future[Option[OrganisationResponse]] ={
+    val auth:Future[Option[AuthResponse]] =  authService.validate(token)
     ???
   }
 }
