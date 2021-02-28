9a16e2acdab1ba4f81ed0854c172cda89d98d784
Muyinda ROgers <moverr@gmail.com>
2021-02-28 04:58:10 +0300

    :Debuggigng

--- app/services/OrganizationService.scala
+++ app/services/OrganizationService.scala
@@ -1,24 +1,26 @@
 package services
 
 import controllers.requests.OrganisationRequest
-import controllers.responses.AuthResponse
+import controllers.responses.{AuthResponse, OrganisationResponse}
 import daos.{OrganisationDAO, UserDao}
 import javax.inject.{Inject, Singleton}
 import utitlities.JwtUtility
 
+import scala.concurrent.Future
+
 @Singleton
 class OrganizationService  @Inject()(organisationDAO: OrganisationDAO)  {
-  OrganisationResponse
+
   //todo: create organisation
-  def create(authorization:AuthResponse,request:OrganisationRequest): Unit ={
+  def create(authorization:AuthResponse,request:OrganisationRequest): Future[OrganisationResponse] ={
     ???
   }
   //todo: list organinsations
-  def list(authorization:AuthResponse,limit:Int, offset:Int): Unit = {
+  def list(authorization:AuthResponse,limit:Int, offset:Int): Future[Seq[OrganisationResponse]]  = {
     ???
   }
   //todo: Get Organization
-  def get(authorization:AuthResponse,id:Int,): Unit ={
+  def get(authorization:AuthResponse,id:Int,): Future[Option[OrganisationResponse]] ={
     ???
   }
 }
