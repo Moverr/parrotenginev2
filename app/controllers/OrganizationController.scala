64a59508808e8bee50e464202b36df777a282ac8
Muyinda ROgers <moverr@gmail.com>
2021-03-01 22:03:54 +0300

    :Debuggging

--- app/controllers/OrganizationController.scala
+++ app/controllers/OrganizationController.scala
@@ -1,13 +1,20 @@
 package controllers
 
+import play.api.mvc._
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, ControllerComponents}
+import play.mvc.Action
 import services.OrganizationService
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 @Singleton
-class OrganizationController @Inject()(cc: ControllerComponents,orgservice: OrganizationService) extends AbstractController(cc){
+class OrganizationController  @Inject()(cc: ControllerComponents,orgservice: OrganizationService)
+  extends AbstractController(cc){
 
   //todo: create Organization
+
 
   //todo: list Organization
 
