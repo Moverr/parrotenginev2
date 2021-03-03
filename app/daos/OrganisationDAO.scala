f009980cbc66221c5e05357544ed6e3f59c73b31
Muyinda ROgers <moverr@gmail.com>
2021-03-03 13:03:43 +0300

    :ebuggging

--- app/daos/OrganisationDAO.scala
+++ app/daos/OrganisationDAO.scala
@@ -41,7 +41,7 @@ class OrganisationDAO  @Inject()(dbConfigProvider: DatabaseConfigProvider) exten
   * Create Organisation
  */
     def createOrganisation(name:String,details:String,owner:Long): Future[Organization] =
-    db.run(orgTable.returning(orgTable) += Organization(0L,name,details,owner, Timestamp.from(LocalDateTime.now().toDateTime),null,null,null))
+    db.run(orgTable.returning(orgTable) += Organization(0L,name,details,owner, new Timestamp(""),null,null,null))
 
 
 }
