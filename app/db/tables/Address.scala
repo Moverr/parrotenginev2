3f95574a38595518267165a22f0d360651513a68
Muyinda ROgers <moverr@gmail.com>
2021-03-05 02:14:42 +0300

    :Debuggging

--- app/db/tables/Address.scala
+++ app/db/tables/Address.scala
@@ -1,7 +1,9 @@
 package db.tables
 
 import java.sql.Timestamp
+
 import slick.jdbc.PostgresProfile.api._
+import slick.sql.SqlProfile.ColumnOption.SqlType
 //import defaults ::
 import slick.lifted.TableQuery
 import slick.jdbc.JdbcProfile
@@ -15,5 +17,21 @@ case class Address (id:Long = 0L,address_type:String,name:String,details:String,
 
 
 class AddressTable(tag: Tag) extends  Table[Address](tag,"addreses"){
+
+  def id       = column[Long]("id",O.PrimaryKey,O.AutoInc)
+  def address_type = column[String]("address_type")
+  def name = column[String]("name")
+  def details = column[String]("details")
+  def parent_type = column[String]("parent_type")
+  def parent_id = column[Long]("parent_id")
+
+  def date_created = column[Timestamp]("date_created", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+  def date_updated = column[Timestamp]("date_updated", SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+  def author_id =  column[Long]("author_id")
+  def updated_by =  column[Long]("updated_by")
+
+
+  override def * = (id,address_type,name,details,parent_type,parent_id,date_created,date_updated,author_id,updated_by).mapTo[Address]
+
 
 }
