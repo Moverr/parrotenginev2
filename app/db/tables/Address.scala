5b23fe7f8c88c4141904aa2a0861e7d5ac9e6ae1
Muyinda ROgers <moverr@gmail.com>
2021-03-05 02:12:02 +0300

    :Debuggging t

--- app/db/tables/Address.scala
+++ app/db/tables/Address.scala
@@ -1,7 +1,19 @@
 package db.tables
 
 import java.sql.Timestamp
+import slick.jdbc.PostgresProfile.api._
+//import defaults ::
+import slick.lifted.TableQuery
+import slick.jdbc.JdbcProfile
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.TableQuery
+
 
 case class Address (id:Long = 0L,address_type:String,name:String,details:String,parent_type:String,parent_id:Long,
                     date_created:Timestamp,author_id:Long, date_updated:Timestamp,updated_by:Long
                    )
+
+
+class AddressTable(tag: Tag) extends  Table[Address](tag,"addreses"){
+
+}
