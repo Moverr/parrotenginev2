0b36b249962340a328523bee5619de2aa0d6631c
Muyinda ROgers <moverr@gmail.com>
2021-03-05 01:51:13 +0300

    :Debuggigng

---
+++ app/controllers/traits/Contact.scala
@@ -0,0 +1,5 @@
+package controllers.traits
+
+sealed  class Contact{
+  val contactType ="none"
+}
---
+++ app/controllers/traits/EmailContact.scala
@@ -0,0 +1,6 @@
+package controllers.traits
+
+class EmailContact  extends Contact{
+
+  override val contactType ="email"
+}
