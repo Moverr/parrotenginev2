c6d0f93dd7b27f3d716bd2af8caa20a1feb44937
Muyinda ROgers <moverr@gmail.com>
2021-04-18 20:31:25 +0300

    :Debuggging : Test

--- test/controllers/OrganizationControllerTest.scala
+++ test/controllers/OrganizationControllerTest.scala
@@ -7,12 +7,10 @@ import play.api.Mode
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.inject.Injector
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
 import play.api.test.Helpers.contentAsString
 import play.api.test.{FakeRequest, Helpers}
 import services.{AuthService, OrganizationService}
-
-
-
 import play.api.test.Helpers._
 
 
@@ -50,9 +48,13 @@ class OrganizationControllerTest extends PlaySpec {
 
 
     }
+    val jsonBody = Json.parse("{\"name\":\"name\", \"details\":\"details\" }")
+
 
     "Create  Station " in {
-      val response = controller.list(0,6).apply(FakeRequest(Helpers.GET, "/v1/organisation/list").withHeaders(
+      val response = controller.create().apply(FakeRequest(Helpers.POST, "/v1/organisation")
+          .withJsonBody(jsonBody)
+          .withHeaders(
         "Authorization"->"eyJraWQiOiJzZWNyZXRLZXkiLCJhbGciOiJSUzI1NiJ9.a29sYUBnbWFpbC5jb206UEBzc3dvcmQ_MTIz.TisoaWUsGQJIAcAFzpHvaFglEEN0GsC09lOvdL9qWn4V0qhVwUd0NG9_0S2OkvZ9rpzsWUYGfl2q3fN55UxLiOi_Q_ClAfj4Q6Hu8M1B2XYE_F8wZXclychzR98SV7wZK8elrPHgNZC4TILM3q7USJDOubVsG9ghnCEdRGEiqVAnjkdArEt7uP_wx068WfikqoIoJ_XGaijbq8J2Sua2pQYOq3PpADGtJIyGY0AdoribBYJRzruoYiGjYbmbxE1wZujCMllSzl8g1vY9JS7t2Etwec61IzdxTW-qR2T7Pf8UgDAsz4danKo1nh433WqN4H7AEcLy9eFGB7C1tQGDqQ"
       ))
 
