4f4a950fad9cdc784c89d387f534cd755b122a79
Muyinda ROgers <moverr@gmail.com>
2021-04-18 19:42:31 +0300

    :Debuggging :

--- test/controllers/OrganizationControllerTest.scala
+++ test/controllers/OrganizationControllerTest.scala
@@ -38,8 +38,9 @@ class OrganizationControllerTest extends PlaySpec {
 
   val authService:AuthService = new AuthService(userDao)
   "Organization Controller " should {
-    "list" in  {
-      val controller   = new OrganizationController(orgService,authService,Helpers.stubControllerComponents())
+    val controller   = new OrganizationController(orgService,authService,Helpers.stubControllerComponents())
+    "list Stations" in  {
+
       val response = controller.list(0,6).apply(FakeRequest(Helpers.GET, "/v1/organisation/list").withHeaders(
         "Authorization"->"eyJraWQiOiJzZWNyZXRLZXkiLCJhbGciOiJSUzI1NiJ9.a29sYUBnbWFpbC5jb206UEBzc3dvcmQ_MTIz.TisoaWUsGQJIAcAFzpHvaFglEEN0GsC09lOvdL9qWn4V0qhVwUd0NG9_0S2OkvZ9rpzsWUYGfl2q3fN55UxLiOi_Q_ClAfj4Q6Hu8M1B2XYE_F8wZXclychzR98SV7wZK8elrPHgNZC4TILM3q7USJDOubVsG9ghnCEdRGEiqVAnjkdArEt7uP_wx068WfikqoIoJ_XGaijbq8J2Sua2pQYOq3PpADGtJIyGY0AdoribBYJRzruoYiGjYbmbxE1wZujCMllSzl8g1vY9JS7t2Etwec61IzdxTW-qR2T7Pf8UgDAsz4danKo1nh433WqN4H7AEcLy9eFGB7C1tQGDqQ"
       ))
@@ -48,6 +49,15 @@ class OrganizationControllerTest extends PlaySpec {
       bodyText mustBe "ok"
 
 
+    }
+
+    "Create  Station " in {
+      val response = controller.list(0,6).apply(FakeRequest(Helpers.GET, "/v1/organisation/list").withHeaders(
+        "Authorization"->"eyJraWQiOiJzZWNyZXRLZXkiLCJhbGciOiJSUzI1NiJ9.a29sYUBnbWFpbC5jb206UEBzc3dvcmQ_MTIz.TisoaWUsGQJIAcAFzpHvaFglEEN0GsC09lOvdL9qWn4V0qhVwUd0NG9_0S2OkvZ9rpzsWUYGfl2q3fN55UxLiOi_Q_ClAfj4Q6Hu8M1B2XYE_F8wZXclychzR98SV7wZK8elrPHgNZC4TILM3q7USJDOubVsG9ghnCEdRGEiqVAnjkdArEt7uP_wx068WfikqoIoJ_XGaijbq8J2Sua2pQYOq3PpADGtJIyGY0AdoribBYJRzruoYiGjYbmbxE1wZujCMllSzl8g1vY9JS7t2Etwec61IzdxTW-qR2T7Pf8UgDAsz4danKo1nh433WqN4H7AEcLy9eFGB7C1tQGDqQ"
+      ))
+
+      val bodyText: String = contentAsString(response)
+      bodyText mustBe "ok"
     }
   }
 }
