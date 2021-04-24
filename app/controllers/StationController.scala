2bfbbf142e95f3c2d0d71f09a9fb5f9e1a18c09a
Muyinda ROgers <moverr@gmail.com>
2021-04-25 02:36:12 +0300

    :Debuggging

--- app/controllers/StationController.scala
+++ app/controllers/StationController.scala
@@ -1,5 +1,17 @@
 package controllers
 
-class StationController {
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{BaseController, ControllerComponents}
+
+@Singleton
+class StationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController  {
+
+  //todo: Create
+
+  //todo: List
+
+  //todo: Update
+
+  //todo: Archive
 
 }
