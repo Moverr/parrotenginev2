db2d46667ae2a5a989948ecd51d0b6f582480798
Muyinda ROgers <moverr@gmail.com>
2021-05-06 12:26:39 +0300

    :ebuggging

--- app/services/StationService.scala
+++ app/services/StationService.scala
@@ -3,20 +3,14 @@ package services
 import controllers.requests.{OrganisationRequest, StationRequest}
 import controllers.responses.{AuthResponse, OrganisationResponse, StationResponse}
 import daos.{OrganisationDAO, StationDAO}
-
 import db.tables.Station
 import javax.inject.Inject
 
+import scala.concurrent.{Await, Future}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
-import javax.inject.Inject
-
-import scala.concurrent.Future
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ Future}
+import scala.concurrent.duration.Duration
 
 
 class StationService   @Inject()(
@@ -27,23 +21,54 @@ class StationService   @Inject()(
 
   //todo: Create
 
-    def create(authResponse: AuthResponse,organisation_id:Int,request:StationRequest): Either[Nil,Future[StationResponse]]={
-  //  if(authResponse == null )   return  Left(new Exception("Invalid Authentication"))
+    def create(authResponse: AuthResponse,organisation_id:Int,request:StationRequest): Either[java.lang.Throwable,Future[StationResponse]]= {
+      if (authResponse == null) return Left(new Exception("Invalid Authentication"))
 
-    //todo: Get Account Details  ::
-    //  val p: Either[java.lang.Throwable,Future[Option[OrganisationResponse]]] = organizationService.get(authResponse, organisation_id)
-
-      val response = stationDao.create(organisation_id,request)
+      //todo: Get Account Details  ::
+     val response =  organizationService.get(authResponse, organisation_id)
 
 
-      response.flatMap{
-        res=> return Right(Future.successful(populateResponse(res)))
+
+/*
+      match {
+
+        case Right(value) =>
+          val result = Await.result(value, Duration.Inf)
+          result match {
+            case Some(value) => {
+
+              stationDao.create(organisation_id, request).
+                flatMap {
+                  res =>   Right(Future.successful(populateResponse(res)))
+                }
+            }
+            case None => Left(new Exception("Invalid"))
+          }
       }
 
-Left(Nil)
+*/
+      /*
+          value.flatMap {
+          x =>
+            x match {
+              case Some(value) => {
+                stationDao.create(organisation_id, request).
+                  flatMap {
+                    res => return Right(Future.successful(populateResponse(res)))
+                  }
+
+              }
+              case None => return Left(new Exception("Invalid"))
+            }
+        } match {
+          case right: Right =>right
+        }
+       */
+    }
 
 
-  }
+
+
 
   //todo: list
 
