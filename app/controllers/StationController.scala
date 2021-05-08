ef0064eef1dd765c2f64afc0fb500dc879262fca
Muyinda ROgers <moverr@gmail.com>
2021-05-08 22:09:10 +0300

    Stations Controller

--- app/controllers/StationController.scala
+++ app/controllers/StationController.scala
@@ -1,14 +1,26 @@
 package controllers
 
+import controllers.responses.AuthResponse
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{BaseController, ControllerComponents}
+import services.AuthService
+
+import scala.concurrent.Future
+
 
 @Singleton
-class StationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController  {
+class StationController @Inject()(
+                                   val controllerComponents: ControllerComponents
+                                   ,val authService:AuthService
+                                 ) extends BaseController  {
 
   //todo: Create
-  def create(): Unit ={
-    ???
+  def create()= Action.async {  implicit  request =>
+    val authorization:String = request.headers.get("authentication").getOrElse("")
+    val authResponse:AuthResponse = authService.validateTokenv2(authorization)
+
+
+    Future.successful(Ok)
   }
   //todo: List
   def list(): Unit = {
