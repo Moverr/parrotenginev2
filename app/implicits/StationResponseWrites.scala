fa161325e20045c3298a129b4b2a189638bb4594
Muyinda ROgers <moverr@gmail.com>
2021-05-08 23:29:35 +0300

    :Debuguggig :

---
+++ app/implicits/StationResponseWrites.scala
@@ -0,0 +1,18 @@
+package implicits
+
+import controllers.responses.{OrganisationResponse, StationResponse}
+
+import controllers.responses.{ OrganisationResponse}
+import play.api.libs.functional.syntax.{unlift, _}
+import play.api.libs.json.{JsPath, Writes}
+
+
+
+object StationResponseWrites {
+  implicit val stationResponseWrites: Writes[StationResponse] = (
+    (JsPath \ "id").write[Long] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "code").write[String] and
+      (JsPath \ "organization").write[OrganisationResponse]
+    )(unlift(StationResponse.unapply))
+}
