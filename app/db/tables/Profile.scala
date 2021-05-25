8dae5923f7d3be3a89816167b9157ac6a46a1c1b
Muyinda ROgers <moverr@gmail.com>
2021-05-25 16:37:40 +0300

    Profile :

--- app/db/tables/Profile.scala
+++ app/db/tables/Profile.scala
@@ -1,3 +1,5 @@
 package db.tables
 
-case class Profile()
+import java.sql.Timestamp
+
+case class Profile(id:Long,user_id:Long,surname:String,other_names:String,gender:String,profile_type:String,author_id:Long,created_on:Timestamp,updated_by:Long,date_updated:Timestamp)
