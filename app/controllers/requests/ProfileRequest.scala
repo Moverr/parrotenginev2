83333d92e21e9cef64731c7c2a2eda1a64f72caa
Muyinda ROgers <moverr@gmail.com>
2021-05-26 17:57:10 +0300

    Getters and Setters

--- app/controllers/requests/ProfileRequest.scala
+++ app/controllers/requests/ProfileRequest.scala
@@ -4,4 +4,13 @@ import controllers.requests.ProfileType.ProfileType
 
 case class ProfileRequest(surname:String,otherName:String,profileType: ProfileType) {
 
+  private var _x = 0
+  def x = _x
+  def x_=(xa:Int): Int ={
+    _x  = xa
+    x
+  }
+
+
+
 }
