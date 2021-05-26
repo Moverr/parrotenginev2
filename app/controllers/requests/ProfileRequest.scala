33a73136fb2370a92164ed24ac4275b90cfd7a4f
Muyinda ROgers <moverr@gmail.com>
2021-05-26 21:41:50 +0300

    :Debuggigng

--- app/controllers/requests/ProfileRequest.scala
+++ app/controllers/requests/ProfileRequest.scala
@@ -1,23 +1,12 @@
 package controllers.requests
 
-import controllers.requests.ProfileType.ProfileType
-
-case class ProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String) {
-
-  private var _user_id:Int = 0
-  private var _author_id:Int = 0
-
-  //Todo: Add User Ids
-  def user_id = _user_id
-  def  user_id_=(user_id:Int): Unit ={
-    _user_id = user_id
-  }
-
-  //Todo: Add Author Id
-  def author_id = _author_id
-  def  author_id=(author_id:Int): Unit ={
-    _user_id = _author_id
-  }
+import java.util.Date
 
 
-}
+sealed trait ProfileRequest
+
+case class ResidentProfileRequest(surname:String,otherName:String,gender:String,date_of_birth:Date) extends ProfileRequest
+case class GuestProfileRequest(surname:String,otherName:String,gender:String,age:Int) extends ProfileRequest
+
+
+
