ae99e426dbf1e932ae9316e71ddf39e424c1bfdf
Muyinda ROgers <moverr@gmail.com>
2021-05-26 18:36:42 +0300

    :ebuggging Profile

--- app/controllers/requests/ProfileRequest.scala
+++ app/controllers/requests/ProfileRequest.scala
@@ -2,15 +2,22 @@ package controllers.requests
 
 import controllers.requests.ProfileType.ProfileType
 
-case class ProfileRequest(surname:String,otherName:String,profileType: ProfileType) {
+case class ProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String) {
 
-  private var _x = 0
-  def x = _x
-  def x_=(xa:Int): Int ={
-    _x  = xa
-    x
+  private var _user_id:Int = 0
+  private var _author_id:Int = 0
+
+  //Todo: Add User Ids
+  def user_id = _user_id
+  def  user_id_=(user_id:Int): Unit ={
+    _user_id = user_id
   }
 
+  //Todo: Add Author Id
+  def author_id = _author_id
+  def  author_id=(author_id:Int): Unit ={
+    _user_id = _author_id
+  }
 
 
 }
