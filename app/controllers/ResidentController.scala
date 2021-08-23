80ad8ee949ec202733efe609874c94f5253b499e
Moverr <moverr@gmail.com>
2021-08-23 18:16:11 +0300

    :Debugggin :

--- app/controllers/ResidentController.scala
+++ app/controllers/ResidentController.scala
@@ -1,14 +1,24 @@
 package controllers
 
+import controllers.responses.AuthResponse
 import play.api.mvc.AbstractController
 import javax.inject.{Inject, Singleton}
 import play.api.mvc._
+import services.AuthService
 
 @Singleton
-class ResidentController  @Inject()(val cc: ControllerComponents)  extends AbstractController(cc) {
+class ResidentController  @Inject()(
+                                     val cc: ControllerComponents
+                                   ,  val authService: AuthService,
+                                   )  extends AbstractController(cc) {
 
+  //GuestProfileRequest
   //todo: create profile
   def create =Action.async{ implicit  request =>
+    //Authorization
+    val authorization: String = request.headers.get("authentication").getOrElse("")
+    val authResponse: AuthResponse = authService.validateTokenv2(authorization)
+
 
     ???
   }
