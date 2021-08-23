dea1202fefbf15cb33f4d3a78bdb44e620801d81
Moverr <moverr@gmail.com>
2021-08-23 20:14:36 +0300

    :Debuggging :

--- app/controllers/ResidentController.scala
+++ app/controllers/ResidentController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import controllers.requests.OrganisationRequest
 import controllers.responses.AuthResponse
 import play.api.mvc.AbstractController
 import javax.inject.{Inject, Singleton}
@@ -19,6 +20,15 @@ class ResidentController  @Inject()(
     val authorization: String = request.headers.get("authentication").getOrElse("")
     val authResponse: AuthResponse = authService.validateTokenv2(authorization)
 
+    //todo: read the body params
+    val surname = request.body.asJson.get("surname").as[String]
+    val otherName = request.body.asJson.get("otherName").as[String]
+    val profiletype = request.body.asJson.get("profiletype").as[String]
+    val gender = request.body.asJson.get("gender").as[String]
+
+
+    val orgRequest = OrganisationRequest(name, details)
+
 
     ???
   }
@@ -26,11 +36,18 @@ class ResidentController  @Inject()(
   //todo: view list items
   def listByStation( offset: Int, limit: Int,  options:String) = Action.async { implicit request =>
 
+    val authorization: String = request.headers.get("authentication").getOrElse("")
+    val authResponse: AuthResponse = authService.validateTokenv2(authorization)
+
+
     ???
   }
 
   //todo: update the profile
   def update =Action.async{ implicit  request =>
+    val authorization: String = request.headers.get("authentication").getOrElse("")
+    val authResponse: AuthResponse = authService.validateTokenv2(authorization)
+
 
     ???
   }
--- app/controllers/requests/ProfileRequest.scala
+++ app/controllers/requests/ProfileRequest.scala
@@ -5,20 +5,22 @@ import org.joda.time.DateTime
 
 abstract class ProfileRequest{
   def  surname:String
-  def  otherName:String
-  def  profileType:ProfileType
+  def  othername:String
+  def  profiletype:ProfileType
   def  gender:String
 
 }
-
-case class BasicProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]  ) extends  ProfileRequest
+//,user_id:Option[Long]
+case class BasicProfileRequest(surname:String,othername:String,profiletype: ProfileType,gender:String  ) extends  ProfileRequest
 
 //Resident
-case class ResidentProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]  ) extends  ProfileRequest{
+//,user_id:Option[Long]
+case class ResidentProfileRequest(surname:String,othername:String,profiletype: ProfileType,gender:String  ) extends  ProfileRequest{
   val  joinDate:DateTime = DateTime.now()
 }
 //Guest
-case class GuestProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]) extends  ProfileRequest{
+//,user_id:Option[Long]
+case class GuestProfileRequest(surname:String,othername:String,profiletype: ProfileType,gender:String) extends  ProfileRequest{
   val  registerDate:DateTime = DateTime.now()
 
 }
