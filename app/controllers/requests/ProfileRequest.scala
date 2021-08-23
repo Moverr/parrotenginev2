de436caa550651e3f12641550062e32e7a964d61
Moverr <moverr@gmail.com>
2021-08-23 17:37:36 +0300

    :Debuggging :

--- app/controllers/requests/ProfileRequest.scala
+++ app/controllers/requests/ProfileRequest.scala
@@ -1,15 +1,27 @@
 package controllers.requests
 
 import controllers.requests.ProfileType.ProfileType
+import org.joda.time.DateTime
 
-abstract class ProfileRequest
+abstract class ProfileRequest{
+  def  surname:String
+  def  otherName:String
+  def  profileType:ProfileType
+  def  gender:String
 
-case class BasicProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Long = 0) extends  ProfileRequest
+}
+
+case class BasicProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]  ) extends  ProfileRequest
 
 //Resident
-case class ResidentProfileRequest(surname:String,otherName:String,profileType: ProfileType=ProfileType.Resident,gender:String,user_id:Long = 0,adress:AddressRequest) extends  ProfileRequest
+case class ResidentProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]  ) extends  ProfileRequest{
+  val  joinDate:DateTime = DateTime.now()
+}
 //Guest
-case class GuestProfileRequest(surname:String,otherName:String,profileType: ProfileType=ProfileType.Guest,gender:String,user_id:Long = 0) extends  ProfileRequest
+case class GuestProfileRequest(surname:String,otherName:String,profileType: ProfileType,gender:String,user_id:Option[Long]) extends  ProfileRequest{
+  val  registerDate:DateTime = DateTime.now()
+
+}
 
 
 
