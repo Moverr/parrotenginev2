c31c0823007aabc11c6c7c8e76f9e50879743916
Moverr <moverr@gmail.com>
2021-08-30 10:52:37 +0300

    :Debuggging :

--- app/db/tables/Profile.scala
+++ app/db/tables/Profile.scala
@@ -1,16 +1,10 @@
 package db.tables
 
-import java.sql.Timestamp
 
 import java.sql.Timestamp
-
-import slick.jdbc.PostgresProfile.api._
 import slick.sql.SqlProfile.ColumnOption.SqlType
-//import defaults ::
-import slick.lifted.TableQuery
-import slick.jdbc.JdbcProfile
 import slick.jdbc.PostgresProfile.api._
-import slick.lifted.TableQuery
+
 
 case  class Profile(id:Long,user_id:Long,surname:String,other_names:String,gender:String,profile_type:String,author_id:Long,created_on:Timestamp,updated_by:Long,date_updated:Timestamp)
 
---
+++ app/services/ResidentialService.scala
@@ -0,0 +1,16 @@
+package services
+
+import daos.OrganisationDAO
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class ResidentialService  @Inject()(organisationDAO: OrganisationDAO) {
+
+  //todo: create
+
+  //todo: update
+
+  //todo: archive
+
+  //todo:list
+}
