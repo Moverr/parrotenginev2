fb681b0d115c8c0ef24a5ba22a94e50469f4663a
Moverr <moverr@gmail.com>
2021-09-13 22:59:43 +0300

    :Debugugging :

--- test/controllers/ResidentiControllerTest.scala
+++ test/controllers/ResidentiControllerTest.scala
@@ -1,10 +1,14 @@
 package controllers
 
+import daos.{ResidentProfileDAO, StationDAO}
+import org.mockito.Mockito
 import org.scalatestplus.play.PlaySpec
 import play.api.Mode
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.inject.Injector
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Helpers
+import services.{AuthService, ResidentialService, StationService}
 
 class ResidentiControllerTest extends PlaySpec {
 
@@ -12,11 +16,17 @@ class ResidentiControllerTest extends PlaySpec {
   lazy val injector: Injector = appBuilder.injector()
   lazy val dbConfProvider: DatabaseConfigProvider = injector.instanceOf[DatabaseConfigProvider]
 
+  val residentDAO =  Mockito.mock(classOf[ResidentProfileDAO])
+  val authService:AuthService =  Mockito.mock(classOf[AuthService])
+  val stationService:StationService =  Mockito.mock(classOf[StationService])
 
+  val residentService:ResidentialService =   new ResidentialService(residentDAO,stationService);
+
+  val controller   = new ResidentController(Helpers.stubControllerComponents(),authService,residentService)
   "Resident Controller " should  {
     "Create Station  " in {
 
-      //todo: create stattion and move on  
+      //todo: create stattion and move on
     }
   }
 }
