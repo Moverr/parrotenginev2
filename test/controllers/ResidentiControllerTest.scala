6e76411aee506b656156d6f208d7b6f363c7e050
Moverr <moverr@gmail.com>
2021-09-13 21:32:58 +0300

    :Debugging :

--- test/controllers/ResidentiControllerTest.scala
+++ test/controllers/ResidentiControllerTest.scala
@@ -1,7 +1,22 @@
 package controllers
 
 import org.scalatestplus.play.PlaySpec
+import play.api.Mode
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.Injector
+import play.api.inject.guice.GuiceApplicationBuilder
 
 class ResidentiControllerTest extends PlaySpec {
 
+  lazy val appBuilder: GuiceApplicationBuilder = new GuiceApplicationBuilder().in(Mode.Test)
+  lazy val injector: Injector = appBuilder.injector()
+  lazy val dbConfProvider: DatabaseConfigProvider = injector.instanceOf[DatabaseConfigProvider]
+
+
+  "Resident Controller " should  {
+    "Create Station  " in {
+
+      //todo: create stattion and move on  
+    }
+  }
 }
