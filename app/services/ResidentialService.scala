829c2234407822651eb3f47e74e4b1657577e770
Moverr <moverr@gmail.com>
2021-09-17 03:49:49 +0300

    :debugging :

--- app/services/ResidentialService.scala
+++ app/services/ResidentialService.scala
@@ -17,85 +17,101 @@ import scala.util.{Failure, Success}
 
 @Singleton
 class ResidentialService  @Inject()(
-                                     residentDAO: ResidentProfileDAO
-                                   ,   profileDAO: ProfileDAO
-                                     ,stationService: StationService
+																		 residentDAO: ResidentProfileDAO
+																	 ,   profileDAO: ProfileDAO
+																		 ,stationService: StationService
 
-                                   ) {
-
-
-  def saveResidentProfile(profile: Profile): Future[ResidentProfileResponse] ={
-    val x:ResidentProfileResponse = new ResidentProfileResponse("dd","dd","dd","dd",1,null,0l,"se")
-    Future.successful(x)
-  }
-  def create(authResponse: AuthResponse, request:ResidentProfileRequest): Either[java.lang.Throwable,Future[ResidentProfileResponse]]= {
-
-    if (authResponse == null) return Left(new Exception("Invalid Authentication"))
-    val resp:Either[java.lang.Throwable,Future[Option[StationResponse]]] =  stationService.get(authResponse,request.stationid)
-
-    val profile = Profile(0L,None,request.surname,request.othername,request.gender,"RESIDENT",authResponse.user_id,getCurrentTimeStamp,authResponse.user_id,  getCurrentTimeStamp )
-   /// val profileResponse = profileDAO.create(profile)
+																	 ) {
 
 
-    val p = for{
-      future1  <- profileDAO.create(profile).recoverWith{
-        case t:Throwable =>  Future.failed(new Exception("Not Able to create client profile "))
-      }
-      future2 <-   saveResidentProfile(future1).recoverWith{
-        case t:Throwable =>  Future.failed(new Exception("Not Able to create client profile "))
-      }
+	def saveResidentProfile(profile: Profile): Future[Resident] ={
+		val resident:Resident = new Resident(0L,profile.id,1,getCurrentTimeStamp(),1,getCurrentTimeStamp(),1,getCurrentTimeStamp())
+		residentDAO.create(resident)
 
-    } yield future1
+	}
+	def create(authResponse: AuthResponse, request:ResidentProfileRequest): Either[java.lang.Throwable,Future[ResidentProfileResponse]]= {
+
+		if (authResponse == null)  Left(new Exception("Invalid Authentication"))
+		else {
+
+			val resp: Either[java.lang.Throwable, Future[Option[StationResponse]]] = stationService.get(authResponse, request.stationid)
+			resp match {
+				case Left(exc) =>  Left(new Exception(exc.getMessage))
+				case Right(station) =>{
+
+          val stationId:Future[Long] =  for{
+               x <- station.map(y=>y.get)
+          } yield (x.id)
 
 
-    Right(p.map(x=>populateResponse(x)))
+					val profile = Profile(0L, None, request.surname, request.othername, request.gender, "RESIDENT", authResponse.user_id, getCurrentTimeStamp, authResponse.user_id, getCurrentTimeStamp)
 
-  }
+					val record = for {
+						future1 <- profileDAO.create(profile).recoverWith {
+							case t: Throwable => Future.failed(new Exception("Not Able to create client profile "))
+						}
+						future2 <- saveResidentProfile(future1).recoverWith {
+							case t: Throwable => Future.failed(new Exception(t.getMessage))
+						}
 
-  //todo: list the items
+					} yield (future1, future2)
 
-  //todo: get item details
+					Right(record.map(item => populateResponse(item._1, item._2)))
 
-  //todo: get items by station
+				}
+			}
 
-  def populateResponse(  entity:Profile,resident: Resident): ResidentProfileResponse =
-    ResidentProfileResponse(
-      entity.surname
-      ,entity.other_names
-      ,entity.profile_type
-      ,entity.gender
-      ,resident.station_id.toInt
-      ,resident.join_date
-      ,resident.created_on.getTime
-      ,"NA"
 
-    )
+		}
 
-  def populateResponse(  entity:Profile): ResidentProfileResponse =
-    ResidentProfileResponse(
-      entity.surname
-      ,entity.other_names
-      ,entity.profile_type
-      ,entity.gender
-      ,1
-        ,new Timestamp(0l)
-      ,0l
+	}
+
+	//todo: list the items
+
+	//todo: get item details
+
+	//todo: get items by station
+
+	def populateResponse(  entity:Profile,resident: Resident): ResidentProfileResponse =
+		ResidentProfileResponse(
+			entity.surname
+			,entity.other_names
+			,entity.profile_type
+			,entity.gender
+			,resident.station_id.toInt
+			,resident.join_date
+			,resident.created_on.getTime
+			,"NA"
+
+		)
+
+
+
+	def populateResponse(  entity:Profile): ResidentProfileResponse =
+		ResidentProfileResponse(
+			entity.surname
+			,entity.other_names
+			,entity.profile_type
+			,entity.gender
+			,1
+				,new Timestamp(0l)
+			,0l
 ,""
-    )
+		)
 
 
 
-  //todo: invite them to create user credentials
+	//todo: invite them to create user credentials
 
 
 
-  //todo: can achive or activate them depending on the permissions
+	//todo: can achive or activate them depending on the permissions
 
-  //todo: create
+	//todo: create
 
-  //todo: update
+	//todo: update
 
-  //todo: archive
+	//todo: archive
 
-  //todo:list
+	//todo:list
 }
