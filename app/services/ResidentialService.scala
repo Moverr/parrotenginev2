16044c0fa09b2357011f33a0b88ab5bb2762e61d
Moverr <moverr@gmail.com>
2021-09-18 20:42:32 +0300

    :DEbuggging :

--- app/services/ResidentialService.scala
+++ app/services/ResidentialService.scala
@@ -2,115 +2,111 @@ package services
 
 import java.sql.Timestamp
 
-import controllers.requests.{ResidentProfileRequest, StationRequest}
+import controllers.requests.ResidentProfileRequest
 import controllers.responses.{AuthResponse, ResidentProfileResponse, StationResponse}
 import daos._
-import db.tables.{Organization, Profile, Resident, Station}
+import db.tables.{Profile, Resident}
 import helpers.Utilities.getCurrentTimeStamp
 import javax.inject.{Inject, Singleton}
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success}
+import scala.concurrent.Future
 
 
 @Singleton
-class ResidentialService  @Inject()(
-																		 residentDAO: ResidentProfileDAO
-																		 ,   profileDAO: ProfileDAO
-																		 ,stationService: StationService
+class ResidentialService @Inject()(
+                                    residentDAO: ResidentProfileDAO
+                                    , profileDAO: ProfileDAO
+                                    , stationService: StationService
 
-																	 ) extends TResidentialService {
-
-
-	override def saveResidentProfile(profile: Profile): Future[Resident] ={
-		val resident:Resident = new Resident(0L,profile.id,1,getCurrentTimeStamp(),1,getCurrentTimeStamp(),1,getCurrentTimeStamp())
-		residentDAO.create(resident)
-
-	}
-	override def create(authResponse: AuthResponse, request:ResidentProfileRequest): Either[java.lang.Throwable,Future[ResidentProfileResponse]]= {
-
-		if (authResponse == null)  Left(new Exception("Invalid Authentication"))
-		else {
-
-			val resp: Either[java.lang.Throwable, Future[Option[StationResponse]]] = stationService.get(authResponse, request.stationid)
-			resp match {
-				case Left(exc) =>  Left(new Exception(exc.getMessage))
-				case Right(station) =>{
-
-					val stationId:Future[Long] =  for{
-						x <- station.map(y=>y.get)
-					} yield (x.id)
+                                  ) extends TResidentialService {
 
 
-					val profile = Profile(0L, None, request.surname, request.othername, request.gender, "RESIDENT", authResponse.user_id, getCurrentTimeStamp, authResponse.user_id, getCurrentTimeStamp)
+  override def saveResidentProfile(profile: Profile): Future[Resident] = {
+    val resident: Resident = new Resident(0L, profile.id, 1, getCurrentTimeStamp(), 1, getCurrentTimeStamp(), 1, getCurrentTimeStamp())
+    residentDAO.create(resident)
 
-					val record = for {
-						future1 <- profileDAO.create(profile).recoverWith {
-							case t: Throwable => Future.failed(new Exception("Not Able to create client profile "))
-						}
-						future2 <- saveResidentProfile(future1).recoverWith {
-							case t: Throwable => Future.failed(new Exception(t.getMessage))
-						}
+  }
 
-					} yield (future1, future2)
+  override def create(authResponse: AuthResponse, request: ResidentProfileRequest): Either[java.lang.Throwable, Future[ResidentProfileResponse]] = {
 
-					Right(record.map(item => populateResponse(item._1, item._2)))
+    if (authResponse == null) Left(new Exception("Invalid Authentication"))
+    else {
 
-				}
-			}
+      val resp: Either[java.lang.Throwable, Future[Option[StationResponse]]] = stationService.get(authResponse, request.stationid)
+      resp match {
+        case Left(exc) => Left(new Exception(exc.getMessage))
+        case Right(station) => {
 
-		}
-
-	}
-
-	//todo: list the items
-
-	//todo: get item details
-
-	//todo: get items by station
-
-	override def populateResponse(  entity:Profile,resident: Resident): ResidentProfileResponse =
-		ResidentProfileResponse(
-			entity.surname
-			,entity.other_names
-			,entity.profile_type
-			,entity.gender
-			,resident.station_id.toInt
-			,resident.join_date
-			,resident.created_on.getTime
-			,"NA"
-
-		)
+          val stationId: Future[Long] = for {
+            x <- station.map(y => y.get)
+          } yield (x.id)
 
 
+          val profile = Profile(0L, None, request.surname, request.othername, request.gender, "RESIDENT", authResponse.user_id, getCurrentTimeStamp, authResponse.user_id, getCurrentTimeStamp)
 
-	override def populateResponse(  entity:Profile): ResidentProfileResponse =
-		ResidentProfileResponse(
-			entity.surname
-			,entity.other_names
-			,entity.profile_type
-			,entity.gender
-			,1
-			,new Timestamp(0l)
-			,0l
-			,""
-		)
+          val record = for {
+            future1 <- profileDAO.create(profile).recoverWith {
+              case t: Throwable => Future.failed(new Exception("Not Able to create client profile "))
+            }
+            future2 <- saveResidentProfile(future1).recoverWith {
+              case t: Throwable => Future.failed(new Exception(t.getMessage))
+            }
+
+          } yield (future1, future2)
+
+          Right(record.map(item => populateResponse(item._1, item._2)))
+
+        }
+      }
+
+    }
+
+  }
+
+  //todo: list the items
+
+  //todo: get item details
+
+  //todo: get items by station
+
+  override def populateResponse(entity: Profile, resident: Resident): ResidentProfileResponse =
+    ResidentProfileResponse(
+      entity.surname
+      , entity.other_names
+      , entity.profile_type
+      , entity.gender
+      , resident.station_id.toInt
+      , resident.join_date
+      , resident.created_on.getTime
+      , "NA"
+
+    )
 
 
+  override def populateResponse(entity: Profile): ResidentProfileResponse =
+    ResidentProfileResponse(
+      entity.surname
+      , entity.other_names
+      , entity.profile_type
+      , entity.gender
+      , 1
+      , new Timestamp(0l)
+      , 0L
+      , ""
+    )
 
-	//todo: invite them to create user credentials
+
+  //todo: invite them to create user credentials
 
 
+  //todo: can achive or activate them depending on the permissions
 
-	//todo: can achive or activate them depending on the permissions
+  //todo: create
 
-	//todo: create
+  //todo: update
 
-	//todo: update
+  //todo: archive
 
-	//todo: archive
-
-	//todo:list
+  //todo:list
 }
