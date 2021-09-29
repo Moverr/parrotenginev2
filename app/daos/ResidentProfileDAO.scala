b3b3290df2aec2851b13c33dea97f9198e774ac1
Moverr <moverr@gmail.com>
2021-09-29 20:39:05 +0300

    :Debugugging :

--- app/daos/ResidentProfileDAO.scala
+++ app/daos/ResidentProfileDAO.scala
@@ -27,13 +27,11 @@ class ResidentProfileDAO @Inject()(dbConfigProvider: DatabaseConfigProvider) {
 
 
   //todo: list items
-  def list(owner_id: Option[Long], station_id: Option[Long], offset: Int, limit: Int,searchQueury:Option[String]): Future[Seq[(Resident, Profile)]] = {
+  def list(owner_id: Option[Long], station_id: Option[Long], offset: Int, limit: Int,searchQuery:Option[String]): Future[Seq[(Resident, Profile)]] = {
 
     val records = for {
       (resident, profile) <- {
         val record = residentTable join profileTable on (_.profile_id === _.id)
-        //val stationQuery = record.filter(_._1.station_id  === station_id.get)
-
 
         val stationQuery =  for(query<- station_id match {
           case Some(station_id) => record.filter(_._1.station_id  === station_id)
@@ -48,13 +46,13 @@ class ResidentProfileDAO @Inject()(dbConfigProvider: DatabaseConfigProvider) {
         }) yield (query)
 
 
-        val searchQueury = for(query<- searchQueury match {
+        val searchedQuery = for(query<- searchQuery match {
           case Some(searchCriteria) => stationQuery.filter(_._2.surname === s"%$searchCriteria%" ) .filter(_._2.other_names === s"%$searchCriteria%" )
           case None => ownerQuery
         }) yield (query)
 
 
-        searchQueury drop (offset) take (limit)
+        searchedQuery drop (offset) take (limit)
       }
 
     }
