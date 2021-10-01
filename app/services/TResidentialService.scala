bd890927f7c340320ab3078755b82f77dccab282
Moverr <moverr@gmail.com>
2021-10-01 21:48:46 +0300

    :Debugugging :

--- app/services/TResidentialService.scala
+++ app/services/TResidentialService.scala
@@ -1,13 +1,13 @@
 package services
 import controllers.requests.ResidentProfileRequest
-import controllers.responses.{AuthResponse, ResidentProfileResponse}
-import db.tables.{Profile, Resident}
+import controllers.responses.{AuthResponse, ResidentProfileResponse, StationResponse}
+import db.tables.{Profile, Resident, Station}
 
 import scala.concurrent.Future
 
 trait TResidentialService {
 
-  def saveResidentProfile(profile: Profile): Future[Resident]
+  def saveResidentProfile(authResponse: AuthResponse,profile: Profile,stationresponse: StationResponse): Future[Resident]
 
   def create(authResponse: AuthResponse, request: ResidentProfileRequest): Either[Throwable, Future[ResidentProfileResponse]]
 
