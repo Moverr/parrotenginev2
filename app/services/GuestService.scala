6e6547e5b9dcf8bbf9f7fede35144b761fefa2ad
Moverr <moverr@gmail.com>
2021-10-24 06:08:35 +0300

    :DEbuggging :

--- app/services/GuestService.scala
+++ app/services/GuestService.scala
@@ -1,51 +1,52 @@
 package services
 
-import controllers.requests.{BasicProfileRequest, GuestProfileRequest, ProfileRequest, ResidentProfileRequest}
+import controllers.requests.{GuestProfileRequest, ProfileRequest}
 import controllers.responses.ProfileResponse
 import daos.{GuestDAO, ProfileDAO, ResidentProfileDAO}
 import db.tables.{Guest, Profile, Resident}
 import javax.inject.{Inject, Singleton}
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 @Singleton
-class GuestService  @Inject()(
-                               residentDAO: ResidentProfileDAO
-                                      , profileDAO: ProfileDAO
-                                      ,guestDAO: GuestDAO
-                                      , stationService: StationService
+class GuestService @Inject()(
+                              residentDAO: ResidentProfileDAO
+                              , profileDAO: ProfileDAO
+                              , guestDAO: GuestDAO
+                              , stationService: StationService
 
-                             ) {
+                            ) {
 
   //todo create
   //list guests on a given statioon or visitor on a given day
 
   //todo: create
-  def create(request:ProfileRequest): Either[Throwable,ProfileResponse] ={
+  def create(request: ProfileRequest): Either[Throwable, ProfileResponse] = {
     request match {
-      case GuestProfileRequest(surname, othername, profiletype, gender, host_id, registerDate, location) =>{
+      case GuestProfileRequest(surname, othername, profiletype, gender, host_id, registerDate, location) => {
         //todo: check if host exists
         val result: Future[Option[(Resident, Profile)]] = residentDAO.get(host_id)
         //todo: check to see that username and othernames are mandatory
-        if(surname.isEmpty && othername.isEmpty) Left(throw new Exception("Names are mandatory "))
-        else{
+        if (surname.isEmpty && othername.isEmpty) Left(throw new Exception("Names are mandatory "))
+        else {
           //todo: check if there exists a profile of the guest. as in the gueust has ever been there before.
-         val response =  guestDAO.getByProfileName(Some(surname),Some(othername))
-           for{
-             re <- {
+          val response = guestDAO.getByProfileName(Some(surname), Some(othername))
+          for {
+            re <- {
 
-           }
-              val recordresponse =  for {
-                  response:Option[(Guest,Profile)] <- response.map(record=>record)
-           } yield (response.map(x=>(x._1,x._2)))
-
-
-        } yield re
+            }
+            val recordresponse = for {
+              response: Option[(Guest, Profile)] <- response.map(record => record)
+            } yield (response.map(x => (x._1, x._2)))
 
 
-        ???
+          } yield re
+
+
+          ???
+        }
       }
       case _ => Left(new Exception("Something went wrong"))
     }
@@ -57,16 +58,18 @@ class GuestService  @Inject()(
     //6: assigng registration external_id
   }
 
-  def populateResponse(): ProfileResponse ={
+
+  def populateResponse(): ProfileResponse = {
     //GuestResponse
     ???
   }
+
   //update profile info
   //cance information
 
-//  register profile.
-//  // check if  host exists. .
-//  visitation type..
+  //  register profile.
+  //  // check if  host exists. .
+  //  visitation type..
 
 
 }
