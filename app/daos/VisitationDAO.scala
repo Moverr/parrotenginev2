60032c333e5d4dd65b1ada12d0a2bf80beafe5b7
Moverr <moverr@gmail.com>
2021-10-25 18:24:06 +0300

    :visitation DAO;

---
+++ app/daos/VisitationDAO.scala
@@ -0,0 +1,29 @@
+package daos
+
+import db.tables.{Guest, GuestTable, ProfileTable, Visitation, VisitationTable}
+import javax.inject.{Inject, Singleton}
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.lifted.TableQuery
+
+import slick.jdbc.PostgresProfile.api._
+
+
+@Singleton
+class VisitationDAO @Inject()(dbConfigProvider: DatabaseConfigProvider) {
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  val guestTable = TableQuery[GuestTable]
+  val profileTable = TableQuery[ProfileTable]
+
+  val visitationTable = TableQuery[VisitationTable]
+  import dbConfig._
+
+
+  def create(request: Visitation) =   db.run(visitationTable.returning(visitationTable) += request)
+
+  //todo; get visitation on a given host.. day etc. 
+
+
+}
