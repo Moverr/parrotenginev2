aeff2a19bfe0434e8c81a60fbbf3d40b0bfff49f
Moverr <moverr@gmail.com>
2021-11-01 18:57:39 +0300

    REfactory :

--- app/implicits/GuestInvitationResponseWrites.scala
+++ app/implicits/GuestInvitationResponseWrites.scala
@@ -1,16 +1,16 @@
 package implicits
 
-import controllers.responses.{GeneralProfileResponse, GuestInvitationResponse, ProfileResponse}
-
-
-import play.api.libs.functional.syntax.{unlift,_}
+import  GuestResponseWrites._
+import controllers.responses.{GuestInvitationResponse, GuestResponse}
+import play.api.libs.functional.syntax.{unlift, _}
 import play.api.libs.json.{JsPath, Writes}
+
 
 
 
 object GuestInvitationResponseWrites {
   implicit val guestProfileInvitation: Writes[GuestInvitationResponse] = (
-    (JsPath \ "profile").write[ProfileResponse] and
+    (JsPath \ "profile").write[GuestResponse] and
       (JsPath \ "timein").write[Option[Long]] and
       (JsPath \ "timeout").write[Option[Long]] and
       (JsPath \ "reference_id").write[String] and
