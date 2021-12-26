c581b63a66fee126bcaa86b6dec7e2b380313867
Moverr <moverr@gmail.com>
2021-12-26 11:27:33 +0300

    :Debuggiging :

--- app/daos/StationDAO.scala
+++ app/daos/StationDAO.scala
@@ -1,39 +1,37 @@
 package daos
 
-import java.sql.Timestamp
-
 import controllers.requests.StationRequest
-import controllers.responses.StationResponse
-import db.tables.{Organization, OrganizationTable, Station, StationTable, User, UserTable}
-
-import scala.concurrent.Future
+import db.tables.{OrganizationTable, Station, StationTable}
 import javax.inject.{Inject, Singleton}
-import org.joda.time.{DateTime, DateTimeZone, LocalDateTime}
 import play.api.db.slick.DatabaseConfigProvider
 import slick.jdbc.JdbcProfile
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.TableQuery
 
-@Singleton
-class StationDAO    @Inject()(dbConfigProvider: DatabaseConfigProvider) {
+import scala.concurrent.Future
 
-  private  val dbConfig = dbConfigProvider.get[JdbcProfile]
-  lazy  val stationTable = TableQuery[StationTable]
-  lazy  val organizationTable = TableQuery[OrganizationTable]
+@Singleton
+class StationDAO @Inject()(dbConfigProvider: DatabaseConfigProvider) {
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  lazy val stationTable = TableQuery[StationTable]
+  lazy val organizationTable = TableQuery[OrganizationTable]
 
   import dbConfig._
 
 
   //todo: create station
-  def create(organisation_id:Long, station:StationRequest): Future[Station] = {
-    db.run(stationTable.returning(stationTable) += Station(0L,organisation_id,station.name,station.code))
+  def create(organisation_id: Long, station: StationRequest): Future[Station] = {
+    db.run(stationTable.returning(stationTable) += Station(0L, organisation_id, station.name, station.code))
   }
 
 
   //todo: list station in an organization
-  def list(organization_id: Long,offset:Int,limit:Int):Future[Seq[Station]]= {
-    db.run(stationTable
-      .filter(_.organisation_id === organization_id)
+  def list(organization_id: Long, offset: Int, limit: Int): Future[Seq[Station]] = {
+
+    val query = stationTable.filter(_.organisation_id === organization_id)
+
+    db.run(query
       .drop(offset)
       .take(limit)
       .result)
@@ -41,12 +39,13 @@ class StationDAO    @Inject()(dbConfigProvider: DatabaseConfigProvider) {
 
 
   //todo: Archive Station
-  def archive(organization_id:Int,id:Int):Unit ={
-  ???
+  def archive(organization_id: Int, id: Int): Unit = {
+    ???
   }
 
-  def get(id:Long):Future[Option[Station]]={
+  def get(id: Long): Future[Option[Station]] = {
     db.run(stationTable.filter(_.id === id).result.headOption)
   }
+
   //todo: Populate Response
 }
