4bedbe4622b6d75c9471eca9085bca05fd6790c7
Moverr <moverr@gmail.com>
2022-04-12 22:07:56 +0300

    :Debuggging :

--- app/services/VisitationService.scala
+++ app/services/VisitationService.scala
@@ -14,6 +14,8 @@ import scala.concurrent.impl.Promise
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
+import scala.concurrent.duration._
+
 
 @Singleton
 class VisitationService @Inject()(
@@ -24,80 +26,19 @@ class VisitationService @Inject()(
 
                                  ) {
 
-  /*
-  1 : You dont need authorization to register a visitation
-  2 : you can specify the host
-  3 : The host will be notified of a visitor through channels like ....
-
-      //1: u dont need authorization
-      //2: check to see that the host id exists
-      //3: check to see that guestProfile for guest exists.
-      //4: create guestProfile if not exists
-      //5: register vistor in the book register
-      //6: assigng registration external_id
-   */
-
-
-
-  def createVisitationA(): Unit ={
-
-  }
-
-
 
   def createGuestInvitation(request: VisitationRequest): Either[Throwable, Future[GuestInvitationResponse]] = {
 
-
-    // val respoonse =  guestDAO.getByProfileName(Some(request.profile.surname),Some(request.profile.othername))
-
-
     request match {
-      case    VisitationRequest(profile, host_id, registerDate, location, stationId, kiosk_id) => {
-        //todo: check if host exists ?? jump this
+      case VisitationRequest(profile, host_id, registerDate, location, stationId, kiosk_id) => {
 
 
-        val response: Option[(Guest, Profile)] = Await.result(guestDAO.getByProfileName(Some(profile.surname), Some(profile.othername)), Duration.Inf)
-
-     val guestProofile:Future[Option[(Guest,Profile)]]  =   guestDAO.getByProfileName(Some(profile.surname), Some(profile.othername))
-
-
-        guestProofile.onComplete{
-          case Success(guestProfileResponse) =>{
-
-          }
-          case Failure(exception) => Left(throw new Exception("Not able to retrievev profile information"))
-        }
-
-
-
-
-        //val xt = guestDAO.getByProfileName(Some(profile.surname), Some(profile.othername))
-
-        /*
-        for{
-
-          response <- guestDAO.getByProfileName(Some(profile.surname), Some(profile.othername))
-          xt  =  response match {
-            case Some(value) => {
-              val visitation = Visitation(0L, value._1.profile_id, host_id, Some(getCurrentTimeStamp()), None, Some(stationId), Some(kiosk_id), Some("pending"), Utilities.RandomString())
-               createVisitation(visitation).map(x => populateResponse(value._2, value._1, x))
-            }
-            case None =>{
-              val record: Future[(Guest, Profile)] = CreateGuestProfile(request)
-              Right(record)
-            }
-          }
-        } yield (xt)
-
-        */
-        //todo: create a  guestProfile if does not exist.
-        // else continue.
+        val response: Option[(Guest, Profile)] = Await.result(guestDAO.getByProfileName(Some(profile.surname), Some(profile.othername)), 2.seconds)
 
 
         response match {
           case Some(value: (Guest, Profile)) => {
-            //todo: create guestProfile
-            //             visitation = Visitation(0L, value._1.id, host_id, Some(getCurrentTimeStamp()),   Some(Timestamp.valueOf(java.time.LocalDate.now().toString) , java.time.LocalDate.now(), Some(stationId), deviceId,Some("pending"),Utilities.RandomString())
+
             val visitation = Visitation(0L, value._1.profile_id, host_id, Some(getCurrentTimeStamp()), None, Some(stationId), Some(kiosk_id), Some("pending"), Utilities.RandomString())
             val record = for {
               response <- createVisitation(visitation).map(x => populateResponse(value._2, value._1, x))
@@ -111,83 +52,70 @@ class VisitationService @Inject()(
 
             val record: Future[(Guest, Profile)] = CreateGuestProfile(request)
 
-           val reat = for {
-             xt <-{
+            val result = for {
+              record <- {
 
-                   val te = record.value.get.get
+                val te = record.value.get.get
 
-                 val visitation = Visitation(0L, te._1.profile_id, host_id, Some(getCurrentTimeStamp()), None, Some(stationId), Some(kiosk_id), Some("pending"), Utilities.RandomString())
-                 val response = createVisitation(visitation).map(x => populateResponse(te._2, te._1, x))
-                 response
+                val visitation = Visitation(0L, te._1.profile_id, host_id, Some(getCurrentTimeStamp()), None, Some(stationId), Some(kiosk_id), Some("pending"), Utilities.RandomString())
+                val response = createVisitation(visitation).map(x => populateResponse(te._2, te._1, x))
+                response
 
-             }
-            } yield (xt)
+              }
+            } yield (record)
 
-            Right(reat)
+            Right(result)
           }
 
 
         }
-
 
       }
 
     }
   }
 
-    //todo: list visitations
-    def list(authResponse: UserResponse, organisation_id: Option[Int], station_id: Option[Int], kiosk_id: Option[Int], offset: Int, limit: Int): Either[java.lang.Throwable, Future[Seq[GuestInvitationResponse]]] = {
-      if (authResponse == null) return Left(new Exception("Invalid Authentication"))
 
-      Right(
-      visitationDAO.list(organisation_id, station_id, kiosk_id, offset, limit).map(y => y.map(record => populateResponse(record._1._1._1._2.get, record._1._1._1._1._2, record._1._1._1._1._1)))
-      )
-    }
+  def CreateGuestProfile(request: VisitationRequest): Future[(Guest, Profile)] = {
+    val record = for {
+      profile <- profileDAO.create(request.profile.surname, request.profile.othername, request.profile.gender, 0L, None, "GUEST").recoverWith {
+        case exception: Throwable => Future.failed(new Exception(exception.getMessage))
+      }
 
-
-    //todo create
-    //list guests on a given statioon or visitor on a given days
-    def CreateGuestProfile(request: VisitationRequest): Future[(Guest, Profile)] = {
-      val record = for {
-        profile <- profileDAO.create(request.profile.surname, request.profile.othername, request.profile.gender, 0L, None, "GUEST").recoverWith {
+      future2 <- {
+        val guest: Guest = Guest(0L, profile.id, None, getCurrentTimeStamp(), None, getCurrentTimeStamp())
+        guestDAO.create(guest).recoverWith {
           case exception: Throwable => Future.failed(new Exception(exception.getMessage))
         }
-
-        future2 <- {
-          val guest: Guest = Guest(0L, profile.id, None, getCurrentTimeStamp(), None, getCurrentTimeStamp())
-          guestDAO.create(guest).recoverWith {
-            case exception: Throwable => Future.failed(new Exception(exception.getMessage))
-          }
-        }
+      }
 
 
-      } yield (future2, profile)
+    } yield (future2, profile)
 
-      record
-    }
-
-
-    def createVisitation(visitation: Visitation): Future[Visitation] = visitationDAO.create(visitation)
-
-    def populateResponse(guestProfile: Profile, guest: Guest, visitation: Visitation): GuestInvitationResponse = {
-      //GuestResponse
-      val profileResponse = populateResponse1(guestProfile)
-      val response = GuestInvitationResponse(profileResponse, visitation.time_in, visitation.time_out, visitation.reference_id, visitation.status.get)
-      response
-    }
-
-
-    //update guestProfile info
-    //cance information
-
-    //  register guestProfile.
-    //  // check if  host exists. .
-    //  visitation type..
-
-
-    //todo: populate basic guestProfile
-      def populateResponse1(profile: Profile): GuestResponse = GuestResponse(profile.id, profile.surname, profile.other_names, profile.profile_type, profile.gender)
-
-
+    record
   }
+
+  def createVisitation(visitation: Visitation): Future[Visitation] = visitationDAO.create(visitation)
+
+  //todo: list visitations
+  def list(authResponse: UserResponse, organisation_id: Option[Int], station_id: Option[Int], kiosk_id: Option[Int], offset: Int, limit: Int): Either[java.lang.Throwable, Future[Seq[GuestInvitationResponse]]] = {
+    if (authResponse == null) return Left(new Exception("Invalid Authentication"))
+
+    Right(
+      visitationDAO.list(organisation_id, station_id, kiosk_id, offset, limit).map(y => y.map(record => populateResponse(record._1._1._1._2.get, record._1._1._1._1._2, record._1._1._1._1._1)))
+    )
+  }
+
+  def populateResponse(guestProfile: Profile, guest: Guest, visitation: Visitation): GuestInvitationResponse = {
+    //GuestResponse
+    val profileResponse = populateResponse1(guestProfile)
+    val response = GuestInvitationResponse(profileResponse, visitation.time_in, visitation.time_out, visitation.reference_id, visitation.status.get)
+    response
+  }
+
+
+  def populateResponse1(profile: Profile): GuestResponse = GuestResponse(profile.id, profile.surname, profile.other_names, profile.profile_type, profile.gender)
+
+
+}
 
